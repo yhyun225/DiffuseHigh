6c04e11f21f419bca56a910fa9086b640e5b1e55
GeunminHwang <gmh1317@gmail.com>
2024-03-14 10:29:03 +0000

    init

---
+++ video/main.py
@@ -0,0 +1,25 @@
+import argparse
+from models.DiffuseHighVideo import TextToVideoDiffusion
+from omegaconf import OmegaConf
+
+def parse_args():
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str, default="config/test_config.yaml")
+    parser.add_argument("--prompt", type=str, default="A teddy bear is dancing in front of the building", help="Text prompt for video generation",)
+    args = parser.parse_args()
+    
+    return args
+
+def main():
+    args = parse_args()
+    
+    config = OmegaConf.load(args.config)
+    prompt = args.prompt
+    
+    model = TextToVideoDiffusion(config)
+    frames = model.Sample_HR_Video_progress(prompt=prompt)[0]
+
+
+if __name__ == "__main__":
+    main()
